f9194d2ec00b67b34f55be4416a8f5cbd12b4e0e
natalie evans <nmevans7@gmail.com>
2020-06-06 15:20:51 -0600

    updated styles

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,6 @@
 class AddressesController < ApplicationController
   before_action :set_location
+  before_action :set_address, only: [:show, :edit, :update, :destroy]
   
   def index
     @addresses = @location.addresses
@@ -9,8 +10,7 @@ class AddressesController < ApplicationController
   end
 
   def new
-    @trips = Trip.all - @location.trips
-    @addresses = @location.addresses.new
+    @address = @location.addresses.new
   end
   
   def edit
@@ -23,29 +23,36 @@ class AddressesController < ApplicationController
     else
       render :new
     end
+  end
+
 
     def update
-      @address = @address.find(params[:id])
       if @address.update(address_params)
-        redirect_to trip_locations_path
+        redirect_to [@location, @address]
       else
         render :edit
       end
     end
 
     def destroy
-      @location.appointments.find(params[:id]).destroy
+      @address.destroy
       redirect_to location_addresses_path(@location)
     end
-  end
+  
 
   private
 
   def address_params
     params.require(:address).permit(:street, :city, :state, :zip)
   end
-
+  
   def set_location
     @location = Location.find(params[:location_id])
   end
+  
+  def set_address
+    @address = Address.find(params[:id])
+  end
+
+  
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ApplicationRecord
   belongs_to :location
 
   def full_address
-    "#{self.street} #{self.city} #{self.state} #{self.zip}"
+    "#{self.street} #{self.city}, #{self.state} #{self.zip}"
   end
 
 end
